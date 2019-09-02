77e489c4ed14cdab76b506b61ba63070985f230b
chtheolo <chtheolo@gmail.com>
2019-09-03 02:28:28 +0300

    Finishing the Conversion Phase

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <iterator>
 #include <algorithm>
 #include "client.h"
 #include "webserver.h"
 
 #define DataRange 15
+
+void RemovePhase(client *client, int numberOfclients) {
+    int i;
+
+    for (i=0; i<numberOfclients; i++) {
+        client[i].UnservedDataRequests--;
+    }
+}
 
 void initial(int *p_item_popularity) {
     for (int i=0; i<DataRange; i++) {
@@ -17,17 +26,6 @@ std::vector<int> AggregationPhase(int *p_item_popularity, client *Client, int nu
     unsigned u;
     int t_count=0, i, data, max, x;
     std::vector<int> ca;
-
-    struct CA {
-        int data;
-        int slack_time;
-        int U;
-    };
-
-
-    std::list<CA> CAlist;
-    std::list<CA>::iterator it;
-
 
     //an array position corresponds to a data item, i.e. [i] -> i, data item
     //and the array value corresponds to popularity, i.e [i] = x , x popularity
@@ -49,56 +47,15 @@ std::vector<int> AggregationPhase(int *p_item_popularity, client *Client, int nu
         ca.push_back(x); //find the candidates for every Rq
     }
 
-
-    CA ca_final;
-
-    /*i=0;
-    it = CAlist.begin();
-    for(u=0; u<ca.size(); u++) {
-        ca_final.data= ca[u];
-        ca_final.U= Client[i].UnservedDataRequests;
-        ca_final.slack_time = Client[i].DeadLines - t_count - ca_final.U;
-        CAlist.insert(it->data, ca[u]);   
-        std::cout << it->data << std::endl;
-        while(it != CAlist.end() && it->data != ca[u]) {
-            it++;
-        }
-        if(it != CAlist.end()) {
-            CAlist.push_back(ca_final);
-        }
-        else{
-            if(it->slack_time > ca_final.slack_time) {
-                it->slack_time = ca_final.slack_time;
-            }
-            if(it->U > ca_final.U) {
-                it->U = ca_final.U;
-            }
-        }
-        CAlist.push_back(ca_final);
-        it++;
-        i++;
-    }
-
-
-    std::cout << "ok" <<std::endl;
-    it = CAlist.begin();
-    while(it != CAlist.end()) {
-        std::cout << it->data << std::endl;
-        std::cout << it->slack_time<< std::endl;
-        it++;
-    }
-
-    std::cout<< "end" << std::endl;*/
-
     return(ca);    
 }
 
 
 int main (int argc, char *argv[]) {
-    int numberOfClients, i, Item_Popularity[DataRange], t_count=0;;
-    unsigned u;
+    int numberOfClients, i, Item_Popularity[DataRange], t_count=0;
+    unsigned u,x;
     int *p_item_popularity;
-    std::vector<int> ca;
+    std::vector<int> ca, buffer, min_buf;
 
     struct CA {
         int data;
@@ -106,8 +63,8 @@ int main (int argc, char *argv[]) {
         int U;
     };
 
-    std::list<CA> CAlist;
-    std::list<CA>::iterator it;
+    std::list<CA> CAlist, broadcastList;
+    std::list<CA>::iterator it, it_buf, it2;
 
     srand(time(NULL));
 
@@ -117,9 +74,6 @@ int main (int argc, char *argv[]) {
     // call the initial function for Item_Popularity array
     initial(p_item_popularity);
 
-    /*for(i=0; i<DataRange; i++) {
-        std::cout << Item_Popularity[i] << std::endl;
-    }*/
     
     std::cout << "Select the number of clients: " << std::endl;
     std::cin >> numberOfClients;
@@ -151,6 +105,8 @@ int main (int argc, char *argv[]) {
 
     CA ca_final;
 
+
+    /***** Make CA list with merging the same data items  ******/
     i=0;
     for(u=0; u<ca.size(); u++) {
         it = CAlist.begin();
@@ -173,22 +129,126 @@ int main (int argc, char *argv[]) {
         }
         i++;
     }
-
-    for(u=0; u<ca.size(); u++) {
-        std::cout << ca[u] << std::endl;
+    /***************************************************************/
+    
+    std::cout << "\nData Popularity : " << std::endl;
+    for(i=0; i<DataRange; i++) {
+        std::cout << "\t\t  data_item[" << i << "]" << " = " << Item_Popularity[i] << std::endl;
     }
 
-    std::cout << "The list CA: " << std::endl;
+    std::cout <<"\nThe candidates from every request: { ";
+    std::cout << ca[0] ;
+    for(u=1; u<ca.size(); u++) { 
+        std::cout << ", ";
+        std::cout << ca[u];
+    }
+    std::cout << " }" << std::endl;
+
+    std::cout << "\nThe list (after merging the same data_items) CA: { " << std::endl;
     it = CAlist.begin();
+    std::cout << "\ndata_item: " << it->data << std::endl;
+    std::cout << "slack_time: " << it->slack_time << std::endl;
+    std::cout << "Unserved_DataItems: " << it->U << std::endl;
+    it++;
     while(it != CAlist.end()) {
-        std::cout << it->data << std::endl;
+        std::cout << "\n";
+        std::cout << "data_item: " << it->data << std::endl;
+        std::cout << "slack_time: " << it->slack_time << std::endl;
+        std::cout << "Unserved_DataItems: " << it->U << std::endl;
         it++;
     }
+    std::cout << "\n} " << std::endl;
 
-    std::cout << "Data Popularity : " << std::endl;
-    for(i=0; i<DataRange; i++) {
-        std::cout << "[" << i << "]" << " = " << Item_Popularity[i] << std::endl;
+    int count=0;
+    it = CAlist.begin();
+    do{
+        count++;
+        it++;
     }
+    while(it != CAlist.end());
+
+    if( count < myWebServer.channels) {
+        it = CAlist.begin();
+        i=1;
+        for(it=CAlist.begin(); it!=CAlist.end(); it++) {
+            std::cout << "channel[" << i << "] ->";
+            std::cout << "| " << it->data << "|"<< std::endl;
+            i++;
+        }
+
+        RemovePhase(Client, numberOfClients );
+    }
+    else { /*****************   Conversion Phase  ********************/
+
+        CA min;
+
+        for(i=0; i<myWebServer.channels; i++) {
+            //std::cout << "enter the for-loop\n" << std::endl;
+            it = CAlist.begin();
+            it_buf = CAlist.begin();
+            min.data = it->data;
+            min.slack_time = it->slack_time;
+            min.U = it->U;
+            it++;
+
+            //std::cout << "\n" << CAlist.size() << "\n" << std::endl;
+
+            while(it != CAlist.end()){
+                if(it->slack_time < min.slack_time) { // data with min(slack_time)
+                    min.data = it->data;
+                    min.slack_time = it->slack_time;
+                    min.U = it->U;
+                    std::advance(it_buf, std::distance(CAlist.begin(), it));
+                }
+                else if(it->slack_time == min.slack_time) { // if min(slack_time) tie then ->
+                    if(it->U < min.U) {         // min(Unserved items)
+                        min.data = it->data;
+                        min.slack_time = it->slack_time;
+                        min.U = it->U;
+                    }
+                    else if(it->U == min.U) {     // if min(Userved_items) tie then ->
+                        if(it->data < min.data) {  // min(data_id)
+                            min.data = it->data;
+                            min.slack_time = it->slack_time;
+                            min.U = it->U;
+                            std::advance(it_buf, std::distance(CAlist.begin(), it));
+                        }
+                    }
+                }
+                it++;
+            }
+
+            broadcastList.push_back(min);
+            CAlist.erase(it_buf, std::next(it_buf,1));
+        }
+
+
+        it = CAlist.begin();
+        std::cout << "\nThe CAlist with the deleted data :" << std::endl;
+        while(it != CAlist.end()) {
+            std::cout << "\ndata item: ";
+            std::cout << it->data ;
+            std::cout << "\nslack time: ";
+            std::cout << it->slack_time ;
+            std::cout << "\nUserved DataItems: ";
+            std::cout << it->U << std::endl;
+            it++;
+        }
+
+        it = broadcastList.begin();
+        std::cout << "\nThe list with the broadcast data :" << std::endl;
+        while(it != broadcastList.end()) {
+            std::cout << "\ndata item: ";
+            std::cout << it->data ;
+            std::cout << "\nslack time: ";
+            std::cout << it->slack_time ;
+            std::cout << "\nUserved DataItems: ";
+            std::cout << it->U << std::endl;
+            it++;
+        }
+
+    }
+
 
     delete [] Client;
     
